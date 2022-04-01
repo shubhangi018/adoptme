af7ce3a2b2df349d5846184e782c2972a9f3c923
shubhangi018 <deshmukhshubhangi18@gmail.com>
2022-04-01 19:42:54 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 
+
 import 'package:animaladopt/project_imports.dart';
 
 
 Future<void> main() async {
+
   ///This will help to bind all the widgets used in the project
   WidgetsFlutterBinding.ensureInitialized();
 
